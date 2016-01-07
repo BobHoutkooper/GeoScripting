73995a299d478c2a61292cdbb16b06c6dcdd287c
Bob Houtkooper <houtkooper.b@gmail.com>
2016-01-07 11:29:30 +0100

    First commit: added functions of Lesson 2 and main.R to project.

---
+++ R/MinusRaster.R
@@ -0,0 +1,10 @@
+# open raster library
+library(raster)
+# Function to substract 2 rasterLayers
+minusRaster <- function(x, y, plot=FALSE) { 
+	z <- x - y
+	if (plot) {
+		plot(z)
+	}
+	return(z)
+}
